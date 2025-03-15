29a7703a94c49a786b58085837b2630ad3188a38
Spellcro <66977337+Spellcro@users.noreply.github.com>
2025-03-15 11:56:07 +0000

    More updates to music scoring

--- utils/music.utils.js
+++ utils/music.utils.js
@@ -3,38 +3,46 @@
  * @param {Set} rows The set containing all rows
  */
 export const scoreMusic = (rows) => {
+	/** @type {string[]} */
 	const rowArr = Array.from(rows);
 
 	let score = 0;
 
 	rowArr.forEach((r) => {
 		// Named rows
-		if (r === '87654321' || r === '13572468' || r === '15263748' || r === '45362718' || r === '75312468') {
-			score += 4;
+		if (r === '87654321' || r === '13572468' || r === '15263748' || r === '75312468') {
+			score += 2;
 		}
 
-		// Big bells on the front
-		if (r[0] === '8' && r[1] === '7' && r[2] === '6') {
-			score += r[3] === '5' ? 2 : 1;
+		const backCombos = [
+			'4578',
+			'4678',
+			'5478',
+			'5678',
+			'6478',
+			'6578',
+			'2468',
+			'3478',
+			'4321',
+			'5432',
+			'34567',
+			'123456',
+			'876543',
+		];
+
+		if (backCombos.some((c) => r.endsWith(c))) {
+			score += 1;
 		}
 
-		// Big bells at the back
-		if (r[7] === '8' && r[6] === '7' && r[5] === '6') {
-			score += r[4] === '5' ? 2 : 1;
+		const frontCombos = ['1357', '8765', '1234', '2345', '4321', '76543', '345678', '654321'];
+
+		if (frontCombos.some((c) => r.indexOf(c) === 0)) {
+			score += 1;
 		}
 
-		// Little bells off the front
-		if (r[0] === '2' && r[1] === '3' && r[2] === '4') {
-			score += r[3] === '5' ? 2 : 1;
-		}
-
-		// Little bells at the back
-		if (r[7] === '2' && r[6] === '3' && r[5] === '4') {
-			score += r[4] === '5' ? 2 : 1;
-		}
-
-		if (r[7] === '1' && r[6] === '2' && r[5] === '3') {
-			score += r[4] === '4' ? 1 : 0.5;
+		// Tittums-type
+		if (r[1] === '5' && r[3] === '6' && r[4] === '7' && r[5] === '8') {
+			score += 1;
 		}
 	});
 
